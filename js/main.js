5afbac573b6e8ae51f66236de9c0b37e4f510bf2
Sofi Estevez <cirquedusophie@gmail.com>
2019-05-01 02:33:53 -0300

    new for mobile

--- js/main.js
+++ js/main.js
@@ -291,40 +291,7 @@
     preloader: false,
 
     fixedContentPos: false
-	});
-	
-	const copyMailId = document.querySelectorAll('.mail-text');
-
-copyMailId.forEach(copyText => {
-    copyText.addEventListener('click', () => {
-        const selection = window.getSelection();
-        const range = document.createRange();
-        range.selectNodeContents(copyText);
-        selection.removeAllRanges();
-        selection.addRange(range);
-
-        try {
-            document.execCommand('copy');
-            selection.removeAllRanges();
-
-            const mailId = copyText.textContent;
-            copyText.textContent = 'Copiado!';
-            copyText.classList.add('success');
-
-            setTimeout(() => {
-                copyText.textContent = mailId;
-                copyText.classList.remove('success');
-            }, 1000);
-        } catch (e) {
-            copyText.textContent = 'No se pudo copiar :( Probá Ctrl+C!';
-            copyText.classList.add('error');
-
-            setTimeout(() => {
-                errorMsg.classList.remove('show');
-            }, 1200);
-        }
-    });
-});
+  });
 
   function makeTimer() {
 
