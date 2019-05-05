ec16251ebcf76474997178fdaafc09a045c8dd52
Sofi Estevez <cirquedusophie@gmail.com>
2019-05-05 17:06:30 -0300

    copy to clipboard feature

--- js/main.js
+++ js/main.js
@@ -293,6 +293,41 @@
     fixedContentPos: false
   });
 
+	// copy email to clipboard on Hablemos section
+	const copyMailId = document.querySelectorAll('.mail-text');
+
+	copyMailId.forEach(copyText => {
+			copyText.addEventListener('click', () => {
+					const selection = window.getSelection();
+					const range = document.createRange();
+					range.selectNodeContents(copyText);
+					selection.removeAllRanges();
+					selection.addRange(range);
+	
+					try {
+							document.execCommand('copy');
+							selection.removeAllRanges();
+	
+							const mailId = copyText.textContent;
+							copyText.textContent = 'Copied!';
+							copyText.classList.add('success');
+	
+							setTimeout(() => {
+									copyText.textContent = mailId;
+									copyText.classList.remove('success');
+							}, 1000);
+					} catch (e) {
+							copyText.textContent = 'Couldn\'t copy, hit Ctrl+C!';
+							copyText.classList.add('error');
+	
+							setTimeout(() => {
+									errorMsg.classList.remove('show');
+							}, 1200);
+					}
+			});
+	});
+
+
   function makeTimer() {
 
 		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
@@ -320,7 +355,6 @@
 }
 
 setInterval(function() { makeTimer(); }, 1000);
-
 
 })(jQuery);
 
